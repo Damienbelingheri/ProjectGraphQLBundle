417b1411d38b3f4f17a8ed1d67c8088004a5502c
damienBelingheri <belingheridamien@gmail.com>
2021-01-15 12:12:30 +0100

    it work !

---
+++ migrations/Version20210114192318.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210114192318 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE astronaut (id INT AUTO_INCREMENT NOT NULL, grade_id INT DEFAULT NULL, pseudo VARCHAR(255) NOT NULL, INDEX IDX_5DADB6E5FE19A1A8 (grade_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE grade (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE planet (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE planet_astronaut (planet_id INT NOT NULL, astronaut_id INT NOT NULL, INDEX IDX_DC60E91DA25E9820 (planet_id), UNIQUE INDEX UNIQ_DC60E91DD390014D (astronaut_id), PRIMARY KEY(planet_id, astronaut_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE astronaut ADD CONSTRAINT FK_5DADB6E5FE19A1A8 FOREIGN KEY (grade_id) REFERENCES grade (id)');
+        $this->addSql('ALTER TABLE planet_astronaut ADD CONSTRAINT FK_DC60E91DA25E9820 FOREIGN KEY (planet_id) REFERENCES planet (id)');
+        $this->addSql('ALTER TABLE planet_astronaut ADD CONSTRAINT FK_DC60E91DD390014D FOREIGN KEY (astronaut_id) REFERENCES astronaut (id)');
+        $this->addSql('DROP TABLE apartment');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE planet_astronaut DROP FOREIGN KEY FK_DC60E91DD390014D');
+        $this->addSql('ALTER TABLE astronaut DROP FOREIGN KEY FK_5DADB6E5FE19A1A8');
+        $this->addSql('ALTER TABLE planet_astronaut DROP FOREIGN KEY FK_DC60E91DA25E9820');
+        $this->addSql('CREATE TABLE apartment (id INT AUTO_INCREMENT NOT NULL, street_adress VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, country VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, city VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, zipcode VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, build_year INT NOT NULL, size INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE astronaut');
+        $this->addSql('DROP TABLE grade');
+        $this->addSql('DROP TABLE planet');
+        $this->addSql('DROP TABLE planet_astronaut');
+    }
+}
